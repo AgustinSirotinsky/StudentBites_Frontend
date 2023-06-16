fb0cc303c7133e2839438b15f0c5907836eebf25
Agustin Sirotinsky <102998952+AgustinSirotinsky@users.noreply.github.com>
2023-06-16 13:19:42 -0300

    vama

---
+++ react/StudentBites/studentbites/src/App.js
@@ -0,0 +1,21 @@
+import logo from './assets/logo.png';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { NavBarTest } from './assets/Complementos';
+
+
+function App() {
+
+  return (
+    NavBarTest()
+  );
+}
+
+export default App;
