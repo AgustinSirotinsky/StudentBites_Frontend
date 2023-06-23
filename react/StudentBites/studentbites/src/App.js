66be072a3eb0dd652f4325ecfbecf42a9dbe908a
Kevolex <jeansimonkevin05@gmail.com>
2023-06-23 08:17:36 -0300

    Update App.js

--- react/StudentBites/studentbites/src/App.js
+++ react/StudentBites/studentbites/src/App.js
@@ -31,10 +31,7 @@ function App() {
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Nav.Link href="#action1">Home</Nav.Link>
                   <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown
-                    title="Dropdown"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
+                  <NavDropdown title="Dropdown" id={`offcanvasNavbarDropdown-expand-${expand}`}>
                     <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action4">
                       Another action
