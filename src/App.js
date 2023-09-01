ba9bff9dc610b22f765c7b16d7bcbe9487e7370d
Agustin Sirotinsky <agussiro06@gmail.com>
2023-09-01 08:03:30 -0300

    cs

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBarTest from './assets/NavBarTest';
 
 import Principal from './components/Principal';
-<<<<<<< HEAD
 import Feed from './components/Feed';
 
-=======
-import CargarReseña from './components/CargarReseña';
->>>>>>> 053f8fd9ce8169ba597ddb7876c3e3bdd8dc6379
 
 export default function App() {
   return (
@@ -17,11 +13,7 @@ export default function App() {
     <NavBarTest></NavBarTest>
       <Routes>
         <Route path="/" element={<Principal />}></Route>
-<<<<<<< HEAD
         <Route path="/" element={<Feed />}></Route>
-=======
-        <Route path="/cargarReseña/:localId" element={<CargarReseña />}></Route>
->>>>>>> 053f8fd9ce8169ba597ddb7876c3e3bdd8dc6379
       </Routes>
     </BrowserRouter>
   );
