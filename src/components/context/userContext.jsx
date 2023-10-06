a6870c3b613ef9eec1ef2214b18c7f99f11b38b6
Agustin Sirotinsky <agussiro06@gmail.com>
2023-10-06 10:34:00 -0300

    ksdk

---
+++ src/components/context/userContext.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+
+export const UserContext = React.createContext();
+
+const UserProvider = (props) => {
+    const [user, setuser] = React.useState(() => {
+        // Initialize user from LocalStorage or set to an empty array if it doesn't exist
+        const storeduser = localStorage.getItem('user');
+        return storeduser ? JSON.parse(storeduser) : [];
+    });
+
+    useEffect(() => {
+        // Update LocalStorage whenever user changes
+        localStorage.setItem('user', JSON.stringify(user));
+    }, [user]);
+
+    return (
+        <UserContext.Provider value={{ user, setuser }}>
+            {props.children}
+        </UserContext.Provider>
+    );
+}
+
+export default UserProvider;
