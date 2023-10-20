dc1ff2f40b27f43df6b4565982f861b55d72f990
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-20 12:15:29 -0300

    poblacion del local

--- src/assets/LocalContent.js
+++ src/assets/LocalContent.js
@@ -18,31 +18,45 @@ export default function LocalContent() {
       .catch(err => console.log(err));
   }, []);
 
+  /* function ajustarPoblacion(local) {
+    if (local.Poblacion == 1) {
+      local.Poblacion = "baja";
+    }
+    else if (local.Poblacion == 2)
+    {
+      local.Poblacion = "media";
+    }
+    else
+    {
+      local.Poblacion = "alta";
+    }
+  }
+  ajustarPoblacion(local.Poblacion);*/
   return (
     <>
+
       <div className="card mb-3">
         <img src={local.Portada} alt="" />
       </div>
       <div className="text-center">
         <h1>{local.Nombre}</h1>
       </div>
-      <a>{local.ID}</a>
       <div class="container text-center">
   <div class="row mb-2">
     <div class="col bg-gray">Dirección</div>
-    <div class="col bg-gray">Column</div>
+    <div class="col bg-gray">{local.Direccion}</div>
   </div>
   <div class="row mb-2">
     <div class="col">Tipo de comida</div>
-    <div class="col">Column</div>
+    <div class="col">falta</div>
   </div>
   <div class="row mb-2">
     <div class="col bg-gray">Precios</div>
-    <div class="col bg-gray">Column</div>
+    <div class="col bg-gray">{local.Precio}</div>
   </div>
   <div class="row mb-2">
     <div class="col">Concurrencia</div>
-    <div class="col">Column</div>
+    <div class="col"></div>
   </div>
   <div class="row mb-2">
     <div class="col bg-gray">Tardanza de la Comida</div>
