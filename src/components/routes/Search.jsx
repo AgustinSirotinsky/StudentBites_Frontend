0ce1a7288b7313d6ea7a9de0b9ad6ae9138ee079
Agustin Sirotinsky <agussiro06@gmail.com>
2023-11-03 11:08:29 -0300

    borre el boton de search, ahora simplemente escribis y se actualiza

--- src/components/routes/Search.jsx
+++ src/components/routes/Search.jsx
@@ -1,6 +1,6 @@
 //React
 import { Link } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 //Bostrap
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,18 +8,38 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+//Icons
+import { FaBeer } from 'react-icons/fa';
 
 //Other
 
 export default function Search () {
     const [localData, setLocalData] = useState([]);
-    const [local, setLocal] = useState("");
+
+    useEffect(() => {
+        fetch('http://localhost:3000/locales')
+        .then (res => res.json())
+        .then (data=> {
+            setLocalData(data)
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+    },[])
     const handleLocalChange = (e) => {
-        setLocal(e.target.value);
-        console.log(local);
-    };
-    const searchLocal = () => {
-        fetch(`http://localhost:3000/local/nombre/${local}`)
+        console.log(e.target.value)
+        if (e.target.value==""){
+            fetch('http://localhost:3000/locales')
+        .then (res => res.json())
+        .then (data=> {
+            setLocalData(data)
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+        }
+        else {
+        fetch(`http://localhost:3000/local/nombre/${e.target.value}`)
         .then(res => res.json())
         .then(data => {
             console.log(data);
@@ -28,30 +48,31 @@ export default function Search () {
         .catch(error => {
             console.error("Error fetching data:", error);
         });
-    }
+    };
+        }
     return (
         <>
         <div className="d-flex">
           <Form.Control type="text" placeholder="Buscar local" autoComplete="off" onChange={handleLocalChange} />
-          <Button variant="primary" onClick={searchLocal}>Search</Button>
         </div>
         {localData.map((localItem) => (
-          <Card key={localItem.ID} style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={localItem.Portada} />
-            <Card.Body>
-              <Card.Title>{localItem.Nombre}</Card.Title>
-              <Card.Text>
-                Calificacion: {localItem.Calificacion}
-                <br />
-                Contacto: {localItem.Contacto}
-                <br />
-                Direccion: {localItem.Direccion}
-                <br />
-                Precio: {localItem.Precio}
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
+        <Link to={`PageLocal:/${localItem.ID}`} style={{ color: '#000000' }}>
+            <Card key={localItem.ID} style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={localItem.Portada} />
+                <Card.Body>
+                <Card.Title>{localItem.Nombre}</Card.Title>
+                <Card.Text>
+                    Calificacion: {localItem.Calificacion}
+                    <br />
+                    Contacto: {localItem.Contacto}
+                    <br />
+                    Direccion: {localItem.Direccion}
+                    <br />
+                    Precio: {localItem.Precio}
+                </Card.Text>
+                </Card.Body>
+            </Card>
+        </Link>
         ))}
       </>
     );
