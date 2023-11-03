28efb7ce655c997a02e01be900a1ef55a3e3aa72
Agustin Sirotinsky <agussiro06@gmail.com>
2023-11-03 11:23:14 -0300

    filter empezado

--- src/components/routes/Search.jsx
+++ src/components/routes/Search.jsx
@@ -10,12 +10,14 @@ import Card from 'react-bootstrap/Card';
 
 //Icons
 import { FaBeer } from 'react-icons/fa';
+import {MdOutlineFilterList} from 'react-icons/md'
 
 //Other
 
 export default function Search () {
     const [localData, setLocalData] = useState([]);
-
+    let filterOption=false;
+    
     useEffect(() => {
         fetch('http://localhost:3000/locales')
         .then (res => res.json())
@@ -26,6 +28,7 @@ export default function Search () {
             console.error("Error fetching data:", error);
         });
     },[])
+
     const handleLocalChange = (e) => {
         console.log(e.target.value)
         if (e.target.value==""){
@@ -48,12 +51,25 @@ export default function Search () {
         .catch(error => {
             console.error("Error fetching data:", error);
         });
-    };
+        };
+    }
+
+    const changeFilterOption = () => {
+        if (filterOption==true){
+            filterOption=false
+            console.log('Filter option now false')
         }
+        else {
+            filterOption=true
+            console.log('Filter option now true')
+        }
+    }
+
     return (
         <>
         <div className="d-flex">
           <Form.Control type="text" placeholder="Buscar local" autoComplete="off" onChange={handleLocalChange} />
+          <MdOutlineFilterList size="50" onClick={changeFilterOption}/>
         </div>
         {localData.map((localItem) => (
         <Link to={`PageLocal:/${localItem.ID}`} style={{ color: '#000000' }}>
