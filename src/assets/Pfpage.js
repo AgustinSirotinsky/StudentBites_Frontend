96d10e9beb61e02e110eae9acf90dda15313a8b3
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-16 12:19:57 -0300

    user

--- src/assets/Pfpage.js
+++ src/assets/Pfpage.js
@@ -1,24 +1,32 @@
-//React
-import React, { useEffect, useState } from "react";
-import { Link, NavLink } from 'react-router-dom';
-
-//Bootsrap
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+//Context
+import { UserContext } from '../components/context/userContext';
 
+import Ja from './Ja.jpg';
+import './Pfpage.css'; // Archivo CSS para los estilos personalizados
 
-export default function ProfileAspects ()
-{
-    return
-    (
-        <div>
-        <a>oaaaa</a>
-        </div>
+export default function ProfileAspects() {
+    // Suponiendo que tienes estos valores para los contadores
+    const likesCount = 100;
+    const followersCount = 500;
+    const reviewsCount = 20;
+    const { user } = React.useContext(UserContext);
+
+    return (
+        <Container className="profile-container">
+            <div className="profile-picture">
+                <img src={Ja} alt="Profile" className="rounded-circle" />
+                <p className="username">{user.Usuario}</p>
+            </div>
+            <div className="profile-stats">
+                <p>Me gusta: {likesCount}</p>
+                <p>Seguidores: {followersCount}</p>
+                <p>Reseñas: {reviewsCount}</p>
+            </div>
+        </Container>
     );
 }
